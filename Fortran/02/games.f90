470c10dee8e8e63d38c79b954926c3650aad40bd
Manuel Pena <mpena991@gmail.com>
2022-12-20 03:25:16 +0100

    02 2nd part, both in one with select case

--- Fortran/02/games.f90
+++ Fortran/02/games.f90
@@ -16,10 +16,22 @@ module games
         score = self%me + 1 + 3*modulo( self%me - self%you + 1, 3 )
     end function 
 
-    function game_from_code(you_code, me_code) result(g)
+    function game_from_code_1(you_code, me_code) result(g)
         character(len=1), intent(in) :: you_code, me_code
         type(game) :: g
-        g = game(ichar(you_code) - ichar('A'), ichar(me_code) - ichar('X'))
+        integer :: you, me 
+        you = ichar(you_code) - ichar('A')
+        me = ichar(me_code) - ichar('X')
+        g = game(you, me)
+    end function
+
+    function game_from_code_2(you_code, me_code) result(g)
+        character(len=1), intent(in) :: you_code, me_code
+        type(game) :: g
+        integer :: you, me 
+        you = ichar(you_code) - ichar('A')
+        me = modulo( you + ichar(me_code) - ichar('Y'), 3 )
+        g = game(you, me )
     end function
     
 end module
--- Fortran/02/solution.f90
+++ Fortran/02/solution.f90
@@ -5,25 +5,42 @@ program solution
 
     character(len=32) :: filename 
     integer :: u, total_score, io_code
-    character(len=1) :: me_code, you_code
+    character(len=1) :: me_code, you_code, part 
     type(game), allocatable :: g
+    
 
     call get_command_argument(1,filename)
+    call get_command_argument(2,part)
+    
     open(file=filename, newunit = u, status = 'old', action = 'read')
 
-    do
-        read(unit=u, fmt=*, iostat=io_code) you_code, me_code
-        select case( io_code)
-        case(0)
-            g = game_from_code(you_code, me_code)
-            total_score = total_score + g%score()
-        case(iostat_end)
-            exit
-        end select
-    end do
+    select case( part )
+    case('1')
+        do
+            read(unit=u, fmt=*, iostat=io_code) you_code, me_code
+            select case( io_code)
+            case(0)
+                g = game_from_code_1(you_code, me_code)
+                total_score = total_score + g%score()
+            case(iostat_end)
+                exit
+            end select
+        end do
+    case('2')
+        do
+            read(unit=u, fmt=*, iostat=io_code) you_code, me_code
+            select case( io_code)
+            case(0)
+                g = game_from_code_2(you_code, me_code)
+                total_score = total_score + g%score()
+            case(iostat_end)
+                exit
+            end select
+        end do
+    end select 
 
     close(u)
 
-    print '(A,I0)', "Part 1, total score: ", total_score
+    print '(A,A,A,I0)', "Part ", part, ", total score: ", total_score
     
 end program solution
